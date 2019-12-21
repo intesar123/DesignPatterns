65612fcad3d3e46627b8055ba2cd155d47af3ced
Intesar Alam <alam.kir@gmail.com>
2019-12-21 13:20:35 +0530

    SRP21122019

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,38 @@
+﻿using DesignPatterns.SOLID.SingleResponsibilityPrinciple;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1
+            /********************Single Responsibility Principle********************/
+            Journal journal = new Journal();
+            journal.addEntry("I started learning desing pattern");
+            journal.addEntry("Learning some principle to design and develop a software");
+            Console.WriteLine(journal);
+           
+
+            var p = new Persistence();
+
+            //To get the location the assembly normally resides on disk or the install directory
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+    
+            //once you have the path you get the directory with:
+            var directory = System.IO.Path.GetDirectoryName(path);
+            var filename =Path.Combine(directory, "JOURNAL.txt");
+            p.saveFile(journal, filename, true);
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/SOLID/SingleResponsibilityPrinciple/Journal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.SingleResponsibilityPrinciple
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int addEntry(string text)
+        {
+            entries.Add($"{++count}:{text}");
+            return count;
+        }
+        public void removeEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine,entries);
+        }
+        /**********Now the journal class not only keeping the entries but also managing the persistence we might face
+        problems when we want to change the persistence or Journal functionality***********/
+
+        //public void Save(string filename)
+        //{
+        //    File.WriteAllText(filename,ToString());
+        //}
+        //public static Journal Load(string fileName)
+        //{
+        //}
+    }
+
+
+    public class Persistence
+    {
+        public void saveFile(Journal journal, string fileName,bool overwirite)
+        {
+            if (overwirite||!File.Exists(fileName))
+            {
+                File.WriteAllText(fileName,journal.ToString());
+            }
+        }
+    }
+}
+
