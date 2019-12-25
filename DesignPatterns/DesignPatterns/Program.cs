b3d0f3f9273ffe7214127cc7c6f1081cc34e6eb1
Intesar Alam <alam.kir@gmail.com>
2019-12-25 13:05:26 +0530

    25122019

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,37 +1,92 @@
-﻿using DesignPatterns.SOLID.SingleResponsibilityPrinciple;
+﻿using DesignPatterns.SOLID.OpenClosedPrinciple;
+using DesignPatterns.SOLID.SingleResponsibilityPrinciple;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DesignPatterns
 {
     class Program
     {
         static void Main(string[] args)
         {
-            #region 1
+            #region //////////////////////////////////////SOLID Principle//////////////////////////////////////
+            #region 1 Single Responsibility Principle
+            Console.WriteLine("********************Single Responsibility Principle********************");
             /********************Single Responsibility Principle********************/
             Journal journal = new Journal();
             journal.addEntry("I started learning desing pattern");
             journal.addEntry("Learning some principle to design and develop a software");
             Console.WriteLine(journal);
-           
+
 
             var p = new Persistence();
 
             //To get the location the assembly normally resides on disk or the install directory
             string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
-    
+
             //once you have the path you get the directory with:
             var directory = System.IO.Path.GetDirectoryName(path);
-            var filename =Path.Combine(directory, "JOURNAL.txt");
+            var filename = Path.Combine(directory, "JOURNAL.txt");
             p.saveFile(journal, filename, true);
             #endregion
 
+            #region 2 Open closedd loop principle
+            Console.WriteLine("********************Open closedd loop principle********************");
+            /********************Open closedd loop principle********************/
+            var apple = new Product("apple", Color.Green, Size.small);
+            var tree = new Product("tree", Color.Green, Size.medium);
+            var book = new Product("book", Color.Red, Size.small);
+            var pen = new Product("pen", Color.Blue, Size.small);
 
+            Product[] products = { apple, tree, book, pen };
+
+            ProductFilter pf = new ProductFilter();
+            Console.WriteLine("Green products(old) :");
+            //foreach (Product product in pf.FilterBySize(products, Size.small))
+            //{
+            //    Console.WriteLine($"-{product.name} is is small");
+            //}
+            Console.WriteLine("Green products(old) :");
+            foreach (Product product in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($"-{product.name} is is green");
+            }
+
+            Console.WriteLine("Green products(New) :");
+            var newFilter = new NewFilter();
+            foreach(Product product in newFilter.Filter(products,new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($"-{product.name} is is green");
+            }
+
+            Console.WriteLine("Small and Green products(New) :");
+            foreach(var product in newFilter.Filter(products,new AndSpecification<Product>(
+                new ColorSpecification(Color.Green),
+                new SizeSpecification(Size.small)
+                )))
+                {
+                Console.WriteLine($"-{product.name} is is green");
+            }
+            #endregion
+
+            #region 3 Liskov substitution principle
+            /********************Liskov substitution principle********************/
+
+            #endregion
+
+            #region 4 Interface segregation principle
+            /********************Interface segregation principle********************/
+
+            #endregion
+
+            #region 5 Dependency inversion principle
+            /********************Dependency inversion principle********************/
+
+            #endregion
+            #endregion Solid
             Console.ReadKey();
         }
     }
---
+++ DesignPatterns/DesignPatterns/SOLID/OpenClosedPrinciple/Product.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.OpenClosedPrinciple
+{
+    // This principle states that classes should be opened for extention but should be closed for modification
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        small, medium, large, x_large
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.color == color)
+                {
+                    yield return product;
+                }
+            }
+        }
+        // Now after some time we feel that there should be one more filter which filters by both -size and color so again
+        // we will have to copy above code and create new filter like below
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.color == color && product.size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+        //the problem is how can we extend the this without going back and extend the functionality, it also voids the stated principle
+        //so to avoid above rigid functionality we'll use inheritance here we are going to use the enterprise pattern called specification pattern
+
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> specification);
+    }
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.color == color;
+
+        }
+    }
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.size == size;
+        }
+    }
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+    public class NewFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> specification)
+        {
+           foreach(Product item in items)
+            {
+                 if(specification.IsSatisfied(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string name;
+        public Color color;
+        public Size size;
+
+        public Product(string name, Color color, Size size)
+        {
+            this.name = name;
+            this.color = color;
+            this.size = size;
+        }
+    }
+}
