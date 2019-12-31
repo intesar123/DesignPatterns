84af4ff64cb3a2d7e80fbd72ab029567f9c9f7a9
Intesar Alam <alam.kir@gmail.com>
2019-12-31 12:46:14 +0530

    ISP31122019

---
+++ DesignPatterns/DesignPatterns/SOLID/InterfaceSegregationPrinciple/Document.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.InterfaceSegregationPrinciple
+{
+    public class Document
+    {
+        //Interface segregation principle is something that u r likely to encounter in the real world u buld the 
+        //interfaces which are too large, the idea is quite simple the interface should be segregated so nobody 
+        //who implements the iterface need to implement all the methods
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    // Here you will find the problem with the OldFashionedPrinter now u r obliged to implement all the methods
+    // here the idea is people dont pay for the things they dont need
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/SOLID/InterfaceSegregationPrinciple/DocumentNew.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.InterfaceSegregationPrinciple
+{
+    public class DocumentNew
+    {
+        //Interface segregation principle is something that u r likely to encounter in the real world u buld the 
+        //interfaces which are too large, the idea is quite simple the interface should be segregated so nobody 
+        //who implements the iterface need to implement all the methods
+
+    }
+
+    
+    public interface IPrinter
+    {
+        void Print(DocumentNew d);
+    }
+    public interface IScanner
+    {
+        void Scan(DocumentNew d);
+    }
+    public interface IFax
+    {
+        void Fax(DocumentNew d);
+    }
+    public interface IMachineNew:IPrinter,IScanner,IFax
+    {
+      
+    }
+    public class MultiFunctionPrinterNew : IMachineNew
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(DocumentNew d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(DocumentNew d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(DocumentNew d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class OldFashionedPrinterNew : IPrinter, IScanner
+    {
+        public void Print(DocumentNew d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(DocumentNew d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+// Suppose we have already implementations for these methods somewhere else. Here we'll have to use decorator pattern
+//here we can just delegate the printer,scanner and fax
+    public class NewFashionedPrinterNew : IMachineNew
+    {
+        private IScanner scanner;
+        private IFax fax;
+        private IPrinter printer;
+
+        public NewFashionedPrinterNew(IScanner scanner, IFax fax, IPrinter printer)
+        {
+            this.scanner = scanner;
+            this.fax = fax;
+            this.printer = printer;
+        }
+
+        public void Scan(DocumentNew d)
+        {
+            scanner.Scan(d);
+            //decorator
+        }
+        public void Fax(DocumentNew d)
+        {
+            fax.Fax(d);
+            //decorator
+        }
+        public void Print(DocumentNew d)
+        {
+            printer.Print(d);
+            //decorator
+        }
+    }
+}
