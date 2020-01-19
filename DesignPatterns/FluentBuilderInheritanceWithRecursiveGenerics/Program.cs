f6140d5633b0dea07bad1b738e6d04cf5c9763ba
Intesar Alam <alam.kir@gmail.com>
2020-01-19 12:02:55 +0530

    FBIRG19012020

---
+++ DesignPatterns/FluentBuilderInheritanceWithRecursiveGenerics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBuilderInheritanceWithRecursiveGenerics
+{
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+    public class PersonInfoBuilder
+    {
+        protected Person person = new Person();
+
+        public PersonInfoBuilder Call(string Name)
+        {
+            person.Name = Name;
+            return this;
+        }
+    }
+    //Suppose that we got some requirement after some time that we've to add job info so by
+    //following open-closed loop principle we can't modify above class so here we have to create another builder here
+    public class PersonJobBuilder: PersonInfoBuilder
+    {
+        public PersonJobBuilder WorkAs(string Position)
+        {
+            person.Position = Position;
+            return this;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new PersonJobBuilder();
+            /****here by calling Call() we are just degrading PersonJobBuilder to PersonInfoBuilder 
+             so unable to chain WorkAs() method, the problem is return type of both the methods **/
+            //builder.Call("Intesar").WorkAs
+
+            //-- builder.WorkAs("Developer").Call("Intesar"); // didn't got its
+
+            var me = PersonNew.New.Call("Intesar Alam").WorkAs("Developer").Build();
+            Console.WriteLine(me);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/FluentBuilderInheritanceWithRecursiveGenerics/SolProgram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentBuilderInheritanceWithRecursiveGenerics
+{
+    // now now we'll have to replace return type of both Call() and WorkAs() method with something more sophisticated
+    //or more Advance. Now the question is how can we propagate info from derive class to its own base class and how can we do infinitely or recursively?
+    // the answer is recursive generics,here I'm going to build a abstract class which contains the common info. 
+    public class PersonNew
+    {
+        public string Name;
+        public string Position;
+        public class Builder : PersonJobBuilder<Builder>
+        {
+
+        }
+        // how can you expose above builder
+        public static Builder New => new Builder();
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+    public abstract class PersonBluilder
+    {
+        protected PersonNew person=new PersonNew();
+        public PersonNew Build()
+        {
+            return person;
+        }
+    }
+   
+    //SELF- class Foo: Bar<Foo>
+    public class PersonInfoBuilderNew<SELF>:PersonBluilder
+        where SELF: PersonInfoBuilderNew<SELF>
+    {
+        //protected Person person = new Person();
+
+        public SELF Call(string Name)
+        {
+            person.Name = Name;
+            return (SELF)this;
+        }
+    }
+    //Suppose that we got some requirement after some time that we've to add job info so by
+    //following open-closed loop principle we can't modify above class so here we have to create another builder here
+    public class PersonJobBuilder<SELF> : PersonInfoBuilderNew<PersonJobBuilder<SELF>>
+        where SELF: PersonJobBuilder<SELF>
+    {
+        public SELF WorkAs(string Position)
+        {
+            person.Position = Position;
+            return (SELF)this;
+        }
+    }
+}
